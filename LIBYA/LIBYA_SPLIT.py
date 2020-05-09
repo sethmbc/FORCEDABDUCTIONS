61740a5c4c994dad7431d1b959ade9df5f1cf13e
sethmbc <50924817+sethmbc@users.noreply.github.com>
2020-05-09 14:38:33 -0400

    Add files via upload

---
+++ LIBYA/LIBYA_SPLIT.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import pandas as pd
+
+df = pd.read_csv('LIBYA.csv')
+df['sub_event_type'] = df['sub_event_type'].str.replace('\W', '_')
+
+
+# In[6]:
+
+
+df['sub_event_type'].unique()
+
+
+# In[7]:
+
+
+for i, g in df.groupby('sub_event_type'):
+    g.to_csv('{}.csv'.format(i), header=True)
+
+
+# In[ ]:
+
+
+
+
